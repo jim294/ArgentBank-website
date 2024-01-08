1bef6ee41826fd4b3af16d24f37ff329e250ddc7
Jean-Marie <jeanmariedeschamps25@gmail.com>
2024-01-08 13:34:29 +0100

    upgrade errorPage include CSS

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -6,8 +6,8 @@ export function ErrorPage() {
             <main>
                 <section className="errorPage">
                     <h1 className="errorPage__title">404</h1>
-                    <p className="errorPage__text">Oups! La page que vous demandez n'existe pas.</p>
-                    <Link className="errorPage__link" to="/">Page d'accueil</Link>
+                    <p className="errorPage__text">Oops! The page you are requesting does not exist.</p>
+                    <Link className="errorPage__link" to="/">go to home page</Link>
                 </section>
             </main>
         </>
