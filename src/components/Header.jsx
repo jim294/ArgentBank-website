da30ac6c5d7cfbf01e77236d6ceb8b459e09e66b
Jean-Marie <jeanmariedeschamps25@gmail.com>
2024-01-15 15:14:44 +0100

    img too webp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <Link className="main-nav-logo" to="/">
           <img
             className="main-nav-logo-image"
-            src="img/argentBankLogo.png"
+            src="img/argentBankLogo.webp"
             alt="Argent Bank Logo"
           />
           <h1 className="sr-only">Argent Bank</h1>
